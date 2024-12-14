3efc2c51465ffa5827865bab257b7997fa743ec2
Ana B. S. Gonçalves <98134840+Ikahjumma@users.noreply.github.com>
2024-12-14 15:52:30 -0300

    Jogo da sorte

---
+++ Eutenhosorte.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eutenhosorte;
+
+/**
+ *
+ * @author AnaBe
+ */
+public class Eutenhosorte {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
